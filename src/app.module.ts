f8ca074e4e4cef01a14def806c43b5a435cfee58
ThuanHM <hmthuandev@gmail.com>
2023-12-11 21:03:41 +0700

    setup db, env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // MongooseModule.forRoot(
+    //   `mongodb+srv://thuanhm:Nokia123sony@cluster0.qucanwn.mongodb.net/`,
+    // ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_URL'), // Loaded from .ENV
+      }),
+    }),
+    ConfigModule.forRoot({
+      envFilePath: ['.env', '.env.development.local', '.env.development'],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
